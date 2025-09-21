7da6d1fc24752bbdb2fc6423acad66cbb5a74054
Aarchi Singh <a254sing@uwaterloo.ca>
2025-09-20 22:32:55 -0400

    added flight code transform function

--- main.py
+++ main.py
@@ -24,15 +24,41 @@ def parse_airline_data(data_string):
     
     return df
 
+def transform_flight_codes(df):
+   
+    
+    df['FlightCodes'] = df['FlightCodes'].replace('', pd.NA)
+    df['FlightCodes'] = pd.to_numeric(df['FlightCodes'], errors='coerce')
+    
+    print("Original values:")
+    print(df['FlightCodes'].tolist())
+    
+    
+    base_value = 1010
+    
+    for i in range(len(df)):
+        if pd.isna(df.loc[i, 'FlightCodes']):
+            df.loc[i, 'FlightCodes'] = base_value + (i * 10)
+        elif i == 0: 
+            base_value = df.loc[i, 'FlightCodes'] - 10
+    
+    df['FlightCodes'] = df['FlightCodes'].astype(int)
+    
+    print("Transformed FlightCodes values:")
+    print(df['FlightCodes'].tolist())
+    
+    return df
+
 
 def main():
    
     data_string = "Airline Code;DelayTimes;FlightCodes;To_From\nAir Canada (!);[21, 40];20015.0;WAterLoo_NEWYork\n<Air France> (12);[];;Montreal_TORONTO\n(Porter Airways. );[60, 22, 87];20035.0;CALgary_Ottawa\n12. Air France;[78, 66];;Ottawa_VANcouvER\n\"\"\".\.Lufthansa.\.\"\"\";[12, 33];20055.0;london_MONTreal\n"
     
     print("--- AIRLINE DATA TRANSFORMATION ---")
-    # print("-"*50)
     
     df = parse_airline_data(data_string)
+    
+    df = transform_flight_codes(df)
     
     return df
 
